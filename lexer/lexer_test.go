1ff0830141559370e6e5c9904940131ff08dc8fe
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-12-29 23:15:50 +0900

    fix test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,20 +7,71 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+
+	input := `let five = 5;
+let ten = 10;
+	
+let add = fn(x, y) {
+	x + y;
+};
+
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		// let five = 5;
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		// let ten = 10;
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		// let add = fn(x, y) {
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		// fn(x, y) {
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		// x, y
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		// )
+		{token.RPAREN, ")"},
+		// {
+		{token.LBRACE, "{"},
+		// x + y;
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		// }
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		// let result = add(five, ten);
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		// add(five, ten);
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		// five, ten
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		// )
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		// EOF
 		{token.EOF, ""},
 	}
 
