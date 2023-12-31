52914f6316192585aabeb82b4f47187e2afcce36
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-12-31 17:26:28 +0900

    add null

---
+++ object/null.go
@@ -0,0 +1,15 @@
+package object
+
+const (
+	NULL_OBJ ObjectType = "NULL"
+)
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
