9ce0264a4dc1d87891a0a0b33036613632a7cc41
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-12-31 22:26:51 +0900

    add boolean object test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -51,3 +51,38 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) {
 			expected, result.Value)
 	}
 }
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true}, {"false", false},
+		//{"1 < 2", true}, {"1 > 2", false}, {"1 < 1", false},
+		//{"1 > 1", false}, {"1 == 1", true}, {"1 != 1", false},
+		//{"1 == 2", false}, {"1 != 2", true},
+		//{"true == true", true}, {"false == false", true},
+		//{"true == false", false}, {"true != false", true},
+		//{"false != true", true}, {"(1 < 2) == true", true},
+		//{"(1 < 2) == false", false}, {"(1 > 2) == true", false},
+		//{"(1 > 2) == false", true},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) {
+
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Fatalf("object is not Boolean. got=%T (%+v)", obj, obj)
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. want=%t, got=%t",
+			expected, result.Value)
+	}
+}
