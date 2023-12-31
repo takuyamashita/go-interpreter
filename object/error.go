88350842db2f1362351afe12b982c52ed1ec13f4
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2024-01-01 00:11:43 +0900

    add error obj

---
+++ object/error.go
@@ -0,0 +1,17 @@
+package object
+
+const (
+	ERROR_OBJ ObjectType = "ERROR"
+)
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType {
+	return ERROR_OBJ
+}
+
+func (e *Error) Inspect() string {
+	return "ERROR: " + e.Message
+}
