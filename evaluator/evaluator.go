83967519d0a8ad6d9f7c1beafc3c0177966180bb
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2024-01-01 18:55:48 +0900

    implement call

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -71,6 +71,21 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 		return &object.Function{Parameters: params, Body: body, Env: env}
 
+	case *ast.CallExpression:
+
+		function := Eval(node.Function, env)
+
+		if isError(function) {
+			return function
+		}
+
+		args := evalExpression(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
+
 	case *ast.ReturnStatement:
 
 		val := Eval(node.ReturnValue, env)
@@ -296,6 +311,66 @@ func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Obje
 		return NULL
 	}
 
+}
+
+func evalExpression(exps []ast.Expression, env *object.Environment) []object.Object {
+
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+
+		result = append(result, evaluated)
+	}
+
+	return result
+}
+
+// call function
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+
+	function, ok := fn.(*object.Function)
+
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	// make new environment for function
+	extendedEnv := extendFunctionEnv(function, args)
+
+	evaluated := Eval(function.Body, extendedEnv)
+
+	// dont stop main thread, we just need to return value
+	// if not unwrap, we get *object.ReturnValue
+	// if unwrap, we get *object.Object
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+
+		// bind argument to local environment
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func isTruthy(obj object.Object) bool {
--- object/environment.go
+++ object/environment.go
@@ -7,11 +7,16 @@ func NewEnvironment() *Environment {
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 
 	obj, ok := e.store[name]
+
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 
 	return obj, ok
 }
@@ -21,4 +26,19 @@ func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 
 	return val
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+
+	// 	stote
+	// 	outer
+	// 		store
+	// 		outer
+	// 			store
+	// 			outer...
+
+	env := NewEnvironment()
+	env.outer = outer
+
+	return env
 }
