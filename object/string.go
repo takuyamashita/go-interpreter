3606ea805803ec314adb9bf976bf7a54486c732f
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2024-01-02 04:10:57 +0900

    add string obj

---
+++ object/string.go
@@ -0,0 +1,17 @@
+package object
+
+const (
+	STRING_OBJ = "STRING"
+)
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType {
+	return STRING_OBJ
+}
+
+func (s *String) Inspect() string {
+	return s.Value
+}
